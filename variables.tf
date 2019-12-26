b82425c39338e2a06aa9af9d60cae052a7f9ded2
Sergio Sisternes <sesispla@outlook.com>
2019-12-26 18:20:52 +0100

    variables formatting

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable resource_group {
 }
 
 variable kubernetes_version {
-  type = string
+  type        = string
   description = "(Required) Version of Kubernetes specified when creating the AKS managed cluster. If not specified, the latest recommended version will be used at provisioning time (but won't auto-upgrade). NOTE: Upgrading your cluster may take up to 10 minutes per node."
 }
 
@@ -36,25 +36,25 @@ variable creator {
 }
 
 variable client_id {
-  type = string
+  type        = string
   description = "client_id - (Required) The Client ID for the Service Principal."
 }
 
 variable client_secret {
-  type = string
+  type        = string
   description = "(Required) The Client Secret for the Service Principal."
 }
 
 variable node_resource_group {
-  type = string
+  type        = string
   description = "(Optional) The name of the Resource Group where the Kubernetes Nodes should exist. Changing this forces a new resource to be created. If empty, this module will generate a friendly name"
-  default = ""
+  default     = ""
 }
 
 variable node_public_ip_enable {
-  type = bool
+  type        = bool
   description = "(Optional) Should nodes in this Node Pool have a Public IP Address? Defaults to false."
-  default = false
+  default     = false
 }
 
 variable dns_prefix {
@@ -76,45 +76,45 @@ variable node_pool_count {
 }
 
 variable node_pool_os_disk_size_gb {
-    type = number
-    description = "(Optional) The size of the OS Disk which should be used for each agent in the Node Pool. Changing this forces a new resource to be created. Default: 60"
-    default = 60  
+  type        = number
+  description = "(Optional) The size of the OS Disk which should be used for each agent in the Node Pool. Changing this forces a new resource to be created. Default: 60"
+  default     = 60
 }
 
 variable "node_pool_max_pods" {
-    type = number
-    description = "(Optional) The maximum number of pods that can run on each agent. Changing this forces a new resource to be created."
-    default = 60
+  type        = number
+  description = "(Optional) The maximum number of pods that can run on each agent. Changing this forces a new resource to be created."
+  default     = 60
 }
 
 variable "node_pool_type" {
-    type = string
-    description = "(Optional) The type of Node Pool which should be created. Possible values are AvailabilitySet and VirtualMachineScaleSets. Defaults to VirtualMachineScaleSets."
-    default = "VirtualMachineScaleSets"
+  type        = string
+  description = "(Optional) The type of Node Pool which should be created. Possible values are AvailabilitySet and VirtualMachineScaleSets. Defaults to VirtualMachineScaleSets."
+  default     = "VirtualMachineScaleSets"
 }
 
 variable "auto_scaling_enable" {
-    type = bool
-    description = "(Optional) Should the Kubernetes Auto Scaler be enabled for this Node Pool? Defaults to false."
-    default = true
+  type        = bool
+  description = "(Optional) Should the Kubernetes Auto Scaler be enabled for this Node Pool? Defaults to false."
+  default     = true
 }
 
 variable "auto_scaling_min_count" {
-    type = number
-    description = "(Optional) The minimum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 100"
-    default = 2
+  type        = number
+  description = "(Optional) The minimum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 100"
+  default     = 2
 }
 
 variable "auto_scaling_max_count" {
-    type = number
-    description = "(Optional) The maximum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 100."
-    default = 8
+  type        = number
+  description = "(Optional) The maximum number of nodes which should exist in this Node Pool. If specified this must be between 1 and 100."
+  default     = 8
 }
 
 variable "kube_dashboard_enabled" {
-    type = bool
-    description = "(Optional) Is the Kubernetes Dashboard enabled?"
-    default = true
+  type        = bool
+  description = "(Optional) Is the Kubernetes Dashboard enabled?"
+  default     = true
 }
 
 variable node_pool_vm_size {
@@ -124,79 +124,79 @@ variable node_pool_vm_size {
 }
 
 variable network_plugin {
-    type        = string
-    description = "(Optional) Network plugin to use for networking. Currently supported values are azure and kubenet. Changing this forces a new resource to be created. Defaults to azure"
-    default     = "azure"
+  type        = string
+  description = "(Optional) Network plugin to use for networking. Currently supported values are azure and kubenet. Changing this forces a new resource to be created. Defaults to azure"
+  default     = "azure"
 }
 
 variable network_docker_bridge_cidr {
-    type        = string
-    description = "(Optional) IP address (in CIDR notation) used as the Docker bridge IP address on nodes. This is required when network_plugin is set to azure. Changing this forces a new resource to be created. Defaults to 172.17.0.1/16"
-    default     = "172.17.0.1/16"
+  type        = string
+  description = "(Optional) IP address (in CIDR notation) used as the Docker bridge IP address on nodes. This is required when network_plugin is set to azure. Changing this forces a new resource to be created. Defaults to 172.17.0.1/16"
+  default     = "172.17.0.1/16"
 }
 
 variable network_policy {
-    type        = string
-    description = "(Optional) Sets up network policy to be used with Azure CNI. Network policy allows us to control the traffic flow between pods. This field can only be set when network_plugin is set to azure. Currently supported values are calico and azure. Changing this forces a new resource to be created. Defaults to calico"
-    default     = "calico"
+  type        = string
+  description = "(Optional) Sets up network policy to be used with Azure CNI. Network policy allows us to control the traffic flow between pods. This field can only be set when network_plugin is set to azure. Currently supported values are calico and azure. Changing this forces a new resource to be created. Defaults to calico"
+  default     = "calico"
 }
 
 variable network_load_balancer_sku {
-    type        = string
-    description = "(Optional) Specifies the SKU of the Load Balancer used for this Kubernetes Cluster. Possible values are basic and standard. Defaults to basic."
-    default     = "basic"
+  type        = string
+  description = "(Optional) Specifies the SKU of the Load Balancer used for this Kubernetes Cluster. Possible values are basic and standard. Defaults to basic."
+  default     = "basic"
 }
 
 variable rbac_enabled {
-    type = bool
-    description = "(Required) Is Role Based Access Control Enabled? Changing this forces a new resource to be created. Defaults to true"
-    default = true
+  type        = bool
+  description = "(Required) Is Role Based Access Control Enabled? Changing this forces a new resource to be created. Defaults to true"
+  default     = true
 }
 
 variable rbac_aad {
-  type = bool
+  type        = bool
   description = "(Optional) Use Azure Active Directory as RBAC system. If not client_app_id is provided, this module will try creating an Azure Application for you"
-  default = false
+  default     = false
 }
 
 variable rbac_aad_admin {
-  type=string
-  description = "(Optional) Default Azure Active Directory user assigned as cluster administrator"  
-  default = null
+  type        = string
+  description = "(Optional) Default Azure Active Directory user assigned as cluster administrator"
+  default     = null
 }
 
 variable rbac_aad_client_app_id {
-  type = "string"
+  type        = string
   description = "(Optional) The Client ID of an Azure Active Directory Application. Changing this forces a new resource to be created."
-  default = null
+  default     = null
 }
 
 variable rbac_aad_server_app_id {
-  type = "string"
+  type        = string
   description = "(Optional) The Server ID of an Azure Active Directory Application. Changing this forces a new resource to be created."
-  default = null
+  default     = null
 }
 
 variable rbac_aad_server_app_secret {
-  type = "string"
+  type        = string
   description = "(Optional) The Server Secret of an Azure Active Directory Application. Changing this forces a new resource to be created."
-  default = null
+  default     = null
 }
 
 variable rbac_aad_tenant_id {
-  type = "string"
+  type        = string
   description = "(Optional) The Tenant ID used for Azure Active Directory Application. If this isn't specified the Tenant ID of the current Subscription is used. Changing this forces a new resource to be created."
-  default = null
+  default     = null
 }
 
 variable linux_admin_username {
-  type = string
+  type        = string
   description = "(Optional) The Admin Username for the Cluster. Changing this forces a new resource to be created. Defaults to cloudcommons"
-  default = "cloudcommons"
+  default     = "cloudcommons"
 }
 
 variable linux_ssh_key {
-  type = string
+  type        = string
   description = "(Required) The Public SSH Key used to access the cluster. Changing this forces a new resource to be created."
 }
 
