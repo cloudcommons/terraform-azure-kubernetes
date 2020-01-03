5b6eb71fbcb3599afca5097512522192fa63f197
Sergio Sisternes <sesispla@outlook.com>
2020-01-03 20:17:55 +0100

    Removed unnecesary output

--- output.tf
+++ output.tf
@@ -1,9 +1,3 @@
-output "client_certificate" {
-  description = "Cluster Client Certificate"
-  sensitive   = true
-  value       = azurerm_kubernetes_cluster.cloudcommons.kube_config.0.client_certificate
-}
-
 output "kube_config" {
   description = "Cluster Kubernetes Configuration object"  
   value       = azurerm_kubernetes_cluster.cloudcommons.kube_config
